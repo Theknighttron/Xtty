ed6cf97f131915df8aae2d721c9cb453252cda88
polyhistor2050 <chrisspaul2050@gmail.com>
2025-03-07 03:24:50 +0300

    Function to generate RSA key pairs

---
+++ internal/common/crypto.go
@@ -0,0 +1,16 @@
+package common
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+)
+
+// Create a new RSA key pair
+func GenerateKeyPair(bits int) (*rsa.PrivateKey, *rsa.PublicKey, error) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return privateKey, &privateKey.PublicKey, nil
+}
