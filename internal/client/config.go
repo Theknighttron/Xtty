ed6b5447d70b7a1b18eb7f8ea093cd9cb439dcca
polyhistor2050 <chrisspaul2050@gmail.com>
2025-03-10 15:07:16 +0300

    Function to create and load user configurations

---
+++ internal/client/config.go
@@ -0,0 +1,45 @@
+package client
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+// Config for client configurations
+type Config struct {
+	Username   string `json:"username"`
+	ServerHost string `json:"server_host"`
+	ServerPort string `json:"server_port"`
+	PrivateKey string `json:"private_key"`
+	PublicKey  string `json:"public_key"`
+}
+
+// Load the Config from the configurations file
+func LoadConfig(configPath string) (*Config, error) {
+	// Create config directory if it doesn't exist
+	configDir := filepath.Dir(configPath)
+	if err := os.MkdirAll(configDir, 0700); err != nil {
+		return nil, err
+	}
+
+	// check if the config file exist
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		return nil, errors.New("config file does not exist")
+	}
+
+	// Read config file
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse config
+	var config Config
+	if err := json.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
