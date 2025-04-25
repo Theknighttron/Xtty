add16d24d9693bf319fb6396844ff8fb402c041f
polyhistor2050 <chrisspaul2050@gmail.com>
2025-04-25 21:57:14 +0300

    Update the code for running the client

--- cmd/xtty-client/main.go
+++ cmd/xtty-client/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/Theknighttron/Xtty/internal/client"
 )
 
 func main() {
-	if err := client.RunClient(); err != nil {
-		log.Fatalf("Client error: %v", err)
+	// Create a new user
+	user := client.NewUser()
+
+	// Generate room code and keys
+	roomCode := client.GenerateRoomCode()
+	if err := user.GenerateKeyPair(); err != nil {
+		log.Fatalf("Failed to generate keys: %v", err)
+	}
+
+	fmt.Printf("Your room code: %s\n", roomCode)
+	fmt.Println("Share this with your peer to connect")
+
+	// Connect to server
+	serverURL := "ws://localhost:8080"
+	if err := user.Connect(serverURL, roomCode); err != nil {
+		log.Fatalf("Connection failed: %v", err)
+	}
+
+	// Send public key
+	if err := user.SendKeyExchange(); err != nil {
+		log.Fatalf("Key exchange failed: %v", err)
+	}
+
+	defer user.Cleanup()
+
+	// Start UI
+	ui := client.NewUI(user)
+	if err := ui.Run(); err != nil {
+		log.Printf("UI error: %v", err)
 	}
 }
